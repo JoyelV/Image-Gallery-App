ebd718d22d31e5632c4bf60eb98c8f5891e27cc6
Joyel <Varghesejoyel71@gmail.com>
2025-02-09 16:35:29 +0530

    2nd commit

--- src/utility/axiosInstance.js
+++ src/utility/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an Axios instance
 const axiosInstance = axios.create({
-  baseURL: "https://image-gallery-backend-plum.vercel.app/api", 
+  baseURL: "http://localhost:5001/api", 
 });
 
 // Request Interceptor
